2981d9aaed6af0b94ea1fbe94c5cd497b0f678cc
Jacob Allen <jahocob@gmail.com>
2015-10-22 14:21:02 -0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,17 @@
+'''CONSTANTS'''
+
+# ask for the amount of elevators and floor
+ELEVS = int(input(" HOW MANY ELEVATORS?   (1-4) "))
+FLOORS = int(input(" HOW MANY FLOORS?    (1-8) "))
+# optional debuging values
+#ELEVS = 1
+#FLOORS = 8
+
+# COLORS
+BLACK    = (   0,   0,   0) 
+WHITE    = ( 255, 255, 255) 
+BLUE     = (   0,   0, 255)
+
+# Screen dimensions
+SCREEN_WIDTH  = 700
+SCREEN_HEIGHT = 500
